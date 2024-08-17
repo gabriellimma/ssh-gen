112e880452486ecb856472b9d2612940c0adc565
gabriellimma <nickspoot@gmail.com>
2024-08-17 20:33:22 -0300

    resolving pipeline errors

--- ssh-gen.sh
+++ ssh-gen.sh
@@ -4,32 +4,32 @@
 clear
 
 # echo some info about the script like its pid, the name and what is for
-echo "\n🚀 running $0 script \n🔎 PID: $$ \n🔐 for generate an ssh key in github."
+printf "\n🚀 running $0 script \n🔎 PID: $$ \n🔐 this script will generate an ssh key in your machine to authenticate you on github.\n"
 
 # reads the email from cli imput and store as a variable
 # inside varEmail for use to generate a  github ssh key later.
-read -p "insert your github account email: " varEmail
+read -r -p "please, insert your github account email: " varEmail
 
 clear
 
 # reads an confirmation imput y,yes,n,no to confirm the email before
 # use it to generate a ssh key.
-read -p "do you confirm your email? \"$varEmail\" [y/n] " varEmailConfirmation
+read -r -p "do you confirm your email? \"$varEmail\" [y/n] " varEmailConfirmation
 
 # get the varEmailConfirmation to upper casen
-varEmailConfirmation=$(echo $varEmailConfirmation | tr 'a-z' 'A-Z')
-echo \[$varEmailConfirmation\]
+varEmailConfirmation=$(printf "$varEmailConfirmation" | tr '[:lower:]' '[:upper:]')
+printf \["$varEmailConfirmation"\]
 
 if [[ $varEmailConfirmation != "Y" ]]
 then
-	echo "run it again to re-enter your email." 
+	printf "\nrun it again to re-enter your email." 
 	exit 1
 else
-	echo "getting ssh key..."
-	#ssh-keygen -t ed25519 -C "$varEmail"
-	echo "\n📎 copying to clipboard..."
-	cat /Users/$USER/.ssh/id_ed25519.pub | pbcopy
-	echo "\n 🚀 now your're ready to go 🚀\n"
+	printf "\ngetting ssh key..."
+	ssh-keygen -t ed25519 -C "$varEmail"
+	printf "\n📎 copying to clipboard..."
+	cat /Users/"$USER"/.ssh/id_ed25519.pub | pbcopy
+	printf "\n 🚀 now your're ready to go 🚀\n"
 	printf '\e]8;;https://github.com/settings/ssh/new\e\\access your github account and paste the key in the ssh key section.\e]8;;\e\\\n'
 	exit 0
 fi
